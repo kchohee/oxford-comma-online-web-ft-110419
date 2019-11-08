e33ade23f30edf55642c5b1099473f634bce710a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-08 20:07:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   array.join(' and ')
 end
 
-def oxford_comma(array)
-  array.first2.join(', ')
-  array.last1.join(' and ')
+def oxford_comma(stuff)
+  stuff.first2.join(', ')
+  stuff.last1.join(' and ')
 end
