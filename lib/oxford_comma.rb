e7967583b0c7e76534af32385aa5a504bbdf70e8
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-08 20:59:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  return array.first if array.size == 1
-  if array.size == 2
+  return array.first if array.length == 1
+  if array.length == 2
     array.insert(1, "and").join(" ")
-  elsif array.size > 2
+  elsif array.length > 2
     array[-1].insert(0, "and ")
     array.join(", ")
   end
